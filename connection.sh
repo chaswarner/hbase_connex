8c1b4b3b00404bca9059ab16134ca230edc11575
Warner <cwarne01@bcbsmamd.net>
2018-11-20 16:20:09 -0500

    different java option

--- connection.sh
+++ connection.sh
@@ -8,6 +8,6 @@ export CLASSPATH=$CLASSPATH:/opt/cloudera/parcels/CDH-5.12.1-1.cdh5.12.1.p0.3/li
 
 export HADOOP_HOME=/opt/cloudera/parcels/CDH
 
-java -Duser.name=csaload1 -cp .:./hbase-connection.jar:/opt/cloudera/parcels/CDH-5.12.1-1.cdh5.12.1.p0.3/lib/hive/lib/hive-jdbc-standalone.jar:/opt/cloudera/parcels/CDH-5.12.1-1.cdh5.12.1.p0.3/lib/hadoop/client/hadoop-common-2.6.0-cdh5.12.1.jar:/opt/cloudera/parcels/CDH-5.12.1-1.cdh5.12.1.p0.3/lib/hadoop/client-0.20/hadoop-core-2.6.0-mr1-cdh5.12.1.jar:/opt/cloudera/parcels/CDH-5.12.1-1.cdh5.12.1.p0.3/lib/hadoop/client/hadoop-auth-2.6.0-cdh5.12.1.jar:/opt/cloudera/parcels/CDH-5.12.2-1.cdh5.12.2.p0.4/lib/hbase/*:$HADOOP_PATH:$HBASE_PATH HBaseClientDemo
+java -Duser.name=csaload1 -cp .:./hbase-1.0-SNAPSHOT.jar:/opt/cloudera/parcels/CDH-5.12.1-1.cdh5.12.1.p0.3/lib/hive/lib/hive-jdbc-standalone.jar:/opt/cloudera/parcels/CDH-5.12.1-1.cdh5.12.1.p0.3/lib/hadoop/client/hadoop-common-2.6.0-cdh5.12.1.jar:/opt/cloudera/parcels/CDH-5.12.1-1.cdh5.12.1.p0.3/lib/hadoop/client-0.20/hadoop-core-2.6.0-mr1-cdh5.12.1.jar:/opt/cloudera/parcels/CDH-5.12.1-1.cdh5.12.1.p0.3/lib/hadoop/client/hadoop-auth-2.6.0-cdh5.12.1.jar:/opt/cloudera/parcels/CDH-5.12.2-1.cdh5.12.2.p0.4/lib/hbase/*:$HADOOP_PATH:$HBASE_PATH HBaseClientDemo
 
 
